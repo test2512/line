33c8b91d0483048254fcb5cd38d80a7e9105263a
test2512 <prasat2512@gmail.com>
2017-06-15 16:31:25 +0700

    Create test2.php

--- test2.php
+++ test2.php
@@ -6,12 +6,15 @@ $channelSecret = '84ef88f48fd35449892287c4569c6a4f';
 $access_token  = 't4dYu+OXU410/rDWhExXMJg90YZE8xueP0VWCCfofcQm+xfTfYOw17UNNMLM1jtcaE7dkt5HnRNBwtpyNtJSz21QMyELyn28U8ctgdg/BOJRBTsc8AxMlhDoi32BZ8uVZ/aFmis97dfE8KKKi5ztlQdB04t89/1O/w1cDnyilFU=';
 
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$bot = new BOT_API($channelSecret, $access_token);
+	
+$bot->sendMessageNew('u25c733ee101047846d54984df0dfb713', 'Hello World !!');
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-$response = $bot->pushMessage('u25c733ee101047846d54984df0dfb713', $textMessageBuilder);
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+if ($bot->isSuccess()) {
+	echo 'Succeeded!';
+	exit();
+}
+
+// Failed
+echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
 exit();
-
-?>
