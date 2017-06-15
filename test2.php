194812631c68a990fe7eb2a61162a2de98692ca3
test2512 <prasat2512@gmail.com>
2017-06-15 16:27:28 +0700

    Create test2.php

--- test2.php
+++ test2.php
@@ -6,7 +6,7 @@ $channelSecret = '84ef88f48fd35449892287c4569c6a4f';
 $access_token  = 't4dYu+OXU410/rDWhExXMJg90YZE8xueP0VWCCfofcQm+xfTfYOw17UNNMLM1jtcaE7dkt5HnRNBwtpyNtJSz21QMyELyn28U8ctgdg/BOJRBTsc8AxMlhDoi32BZ8uVZ/aFmis97dfE8KKKi5ztlQdB04t89/1O/w1cDnyilFU=';
 
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('$access_token');
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
