4902f3fe023ba86904e17d61838fd498d69e89ee
test2512 <prasat2512@gmail.com>
2017-06-15 15:55:59 +0700

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,25 @@
+<?php
+
+include ('line-bot.php');
+
+//$channelSecret = '551ec4feee0.....43cff0';
+$channelSecret = '84ef88f48fd35449892287c4569c6a4f';
+$access_token  = 't4dYu+OXU410/rDWhExXMJg90YZE8xueP0VWCCfofcQm+xfTfYOw17UNNMLM1jtcaE7dkt5HnRNBwtpyNtJSz21QMyELyn28U8ctgdg/BOJRBTsc8AxMlhDoi32BZ8uVZ/aFmis97dfE8KKKi5ztlQdB04t89/1O/w1cDnyilFU=';
+//$access_token  = '2og9ogezC8k.......5ZUEQQdB04t89/1O/w1cDnyilFU=';
+
+$bot = new BOT_API($channelSecret, $access_token);
+	
+if (!empty($bot->isEvents)) {
+		
+    $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+
+    if ($bot->isSuccess()) {
+        echo 'Succeeded!';
+        exit();
+    }
+
+    // Failed
+    echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+    exit();
+
+}
