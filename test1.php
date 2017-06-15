aae87ae7ea27083ee82e268eed66af573821a20b
test2512 <prasat2512@gmail.com>
2017-06-15 16:34:29 +0700

    Create test1.php

--- test1.php
+++ test1.php
@@ -12,9 +12,10 @@ $bot = new BOT_API($channelSecret, $access_token);
 if (!empty($bot->isEvents)) {
 		
 //    $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+      $bot->sendMessageNew('u25c733ee101047846d54984df0dfb713', 'Hello World !!');
+  //  $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
     //$bot->pushMessage('u25c733ee101047846d54984df0dfb713', $textMessageBuilder);	
-    $bot->pushMessage('u840df9eb39827a773726d39a87e973f3', $textMessageBuilder);	
+   // $bot->pushMessage('u840df9eb39827a773726d39a87e973f3', $textMessageBuilder);	
 //	fang 
 //u840df9eb39827a773726d39a87e973f3	
 
